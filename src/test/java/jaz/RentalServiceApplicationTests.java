76c297e6e72e5697d575b3e72cca04e701276ba8
s20500-pj <s20500@pjwstk.edu.pl>
2021-06-05 16:30:01 +0200

    Initial commit

---
+++ src/test/java/jaz/RentalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package jaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
